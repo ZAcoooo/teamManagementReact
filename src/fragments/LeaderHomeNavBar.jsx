d278c1ff610c593687349fc3e017f99d01f13b2d
Qichen Wang <souasgard@outlook.com>
2024-02-19 21:23:21 -0800

    update the leader home navbar

--- src/fragments/LeaderHomeNavBar.jsx
+++ src/fragments/LeaderHomeNavBar.jsx
@@ -6,8 +6,17 @@ class LeaderHomeNavBar extends Component {
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link class="navbar-brand" to="/Leader" style={{ fontSize: "28px" }}>Home</Link>
-          <span>Role: Leader</span>
+          <Link className="navbar-brand" to="/" style={{ fontSize: "28px", fontWeight: "bold" }}>Login Page</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/Leader" style={{ fontSize: "22px" }}>Home</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
