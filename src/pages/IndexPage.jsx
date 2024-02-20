8f69ba421560c2716185eb7684c7a11c4fbaa3bc
Qichen Wang <souasgard@outlook.com>
2024-02-19 18:53:05 -0800

    complete the IndexPage and its css file

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './IndexPage.css';
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <div className='login'>
+      <h1 className='login-title'>Login Page</h1>
+      <Link to="/Leader"><button className='user-button'>Leader</button></Link>
+      <Link to="/Member"><button className='user-button'>Member</button></Link>
+    </div>
+    );
+  }
+}
+
+
+export default IndexPage;
