2ac1e3293c8f77c124f407dcf45bb8513cae37c3
Qichen Wang <souasgard@outlook.com>
2024-02-19 21:27:21 -0800

    edit project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,7 +7,7 @@ export default class ProjectCard extends Component {
         <div className="card-body">
           <h5 className="card-title">Name: Test Project</h5>
           <p className="card-text">Description: This is description of the first test project</p>
-          <p className="card-text">Project Members: Lee, Zaco, Malcolm</p>
+          <p className="card-text">Project Members: Joe, Lee, Zaco, Malcolm, Jason</p>
         </div>
       </div>
     );
