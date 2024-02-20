73960a6e49b24a6632ce3b476e46b240a5cf8a4e
Qichen Wang <souasgard@outlook.com>
2024-02-19 20:06:29 -0800

    fix style problem

--- src/pages/LeaderHome.jsx
+++ src/pages/LeaderHome.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
 
 class LeaderHome extends Component {
-    render () {
-        return (
-            <div>
-                <h1>Leader</h1>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <h1>Leader</h1>
+      </div>
+    );
+  }
 }
 
 export default LeaderHome;
